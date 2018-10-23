102585a3396f38702db8138112edd0482db3fe6d
Michael Blumenthal <bloomer25@googlemail.com>
2018-10-24 00:07:07 +0100

    finishes user class

--- src/main/java/com/codeclan/example/files_and_folders/models/User.java
+++ src/main/java/com/codeclan/example/files_and_folders/models/User.java
@@ -1,4 +1,60 @@
 package com.codeclan.example.files_and_folders.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "users")
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "user")
+    private List<Folder> folders;
+
+    public User(String name){
+        this.name = name;
+        this.folders = new ArrayList<>();
+    }
+
+    public User(){
+
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Folder> getFolders() {
+        return folders;
+    }
+
+    public void setFolders(List<Folder> folders) {
+        this.folders = folders;
+    }
+
+    public void addFolder(Folder folder) {
+       this.folders.add(folder);
+    }
 }
