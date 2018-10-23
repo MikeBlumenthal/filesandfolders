40ed26f7f50f9e184436e51144185b342d50ad5e
Michael Blumenthal <bloomer25@googlemail.com>
2018-10-24 00:30:11 +0100

    adds getters and setters to file class

--- src/main/java/com/codeclan/example/files_and_folders/models/File.java
+++ src/main/java/com/codeclan/example/files_and_folders/models/File.java
@@ -33,4 +33,44 @@ public class File {
 
     public File(){
     }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getExtension() {
+        return extension;
+    }
+
+    public void setExtension(String extension) {
+        this.extension = extension;
+    }
+
+    public double getSize() {
+        return size;
+    }
+
+    public void setSize(double size) {
+        this.size = size;
+    }
+
+    public Folder getFolder() {
+        return folder;
+    }
+
+    public void setFolder(Folder folder) {
+        this.folder = folder;
+    }
 }
